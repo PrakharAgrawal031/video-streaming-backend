5f4731730abc54c7c387ecc6f1fc251b6cbcdead
Prakhar <agrawalprakhar031@gmail.com>
2024-08-03 20:40:13 +0530

    add user.controller routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,32 +1,43 @@
 import { Router } from "express";
-import {registerUser,
-    logoutUser,
-    loginUser,
-    refreshAccessToken,
-    changeUserPassword,
-    getCurrentUser,
-    updateAccountDetails,
-    updateAvtar,
-    updateCoverimage,
-    getUserProfile,
-    getWatchHistory} from '../controllers/user.controller.js'
-import { upload } from '../middlewares/multer.middleware.js';
-import {verifyJWT} from '../middlewares/auth.middleware.js';
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeUserPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvtar,
+  updateCoverimage,
+  getUserProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route('/register').post(
-    upload.fields([
-        { name: 'avtar', maxCount: 1 },
-        { name: 'coverimage', maxCount: 1 }
-    ]),
-    registerUser
+router.route("/register").post(
+  upload.fields([
+    { name: "avtar", maxCount: 1 },
+    { name: "coverimage", maxCount: 1 },
+  ]),
+  registerUser
 );
 
-router.route('/login').post(loginUser)
-router.route('/profile').get(getUserProfile)
+router.route("/login").post(loginUser);
 //secured routes
-router.route('/logout').post(verifyJWT, logoutUser)
-router.route('/refresh-token').post(refreshAccessToken)
-
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeUserPassword);
+router.route("/get-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avtar")
+  .patch(verifyJWT, upload.single("avtar"), updateAvtar);
+router
+  .route("/update-coverimage")
+  .patch(verifyJWT, upload.single("coverimage"), updateCoverimage);
+router.route("/c/:username").get(verifyJWT, getUserProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
